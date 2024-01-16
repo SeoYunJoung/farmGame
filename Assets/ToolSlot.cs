6960d04bac58a79be865fb673dd62fb4c92c69ba
gkdmsgus <gkdmsgus0422@gmail.com>
2024-01-17 01:36:30 +0900

    gkdmsgus
    도구 추가

--- Assets/ToolSlot.cs
+++ Assets/ToolSlot.cs
@@ -3,12 +3,10 @@ using UnityEngine.UI;
 
 public class ToolSlot : MonoBehaviour
 {
-    public Sprite toolSprite; // 이 슬롯에 설정할 도구 스프라이트
+    public Image toolImage; // 이 슬롯에 설정할 도구 이미지
 
-    private void Start()
+    public void SetImage(Sprite sprite)
     {
-        var image = GetComponent<Image>();
-        if (image != null)
-            image.sprite = toolSprite; // 슬롯에 스프라이트 설정
+        toolImage.sprite = sprite; // 슬롯에 스프라이트 설정
     }
 }
