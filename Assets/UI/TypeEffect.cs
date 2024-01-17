095c4b42182bc5a03034d9d3bd1c4ab1f31cde4f
Seo-YunJoung <130148628+SeoYunJoung@users.noreply.github.com>
2024-01-17 20:32:20 +0900

    Revert "타이핑 이펙트 오류 수정"
    This reverts commit ac8184c414e9d22da308951e830d4b4ea9f8ccc4.

--- Assets/UI/TypeEffect.cs
+++ Assets/UI/TypeEffect.cs
@@ -8,9 +8,8 @@ public class TypeEffect : MonoBehaviour
     string targetMsg; //문자열 변수
     public int CharPerSeconds; //문자열 재생 속도
     public GameObject EndCursor;
-    public Text msgText; //대화 텍스트 가져오기
+    Text msgText; //대화 텍스트 가져오기
     int index; //대화 텍스트 index
-    float interval;
 
 
     private void Awake()
@@ -30,9 +29,7 @@ public class TypeEffect : MonoBehaviour
         index = 0;
         EndCursor.SetActive(false); //엔드커서 비활성화
 
-        interval = 1.0f/CharPerSeconds; //1초에 나올 글자 수
-        Debug.Log(interval);
-        Invoke("Effecting", interval); //Effecting 함수 속도만큼 시간 지연
+        Invoke("Effecting", 1/CharPerSeconds); //Effecting 함수 속도만큼 시간 지연
     }
 
     void Effecting()
@@ -45,7 +42,7 @@ public class TypeEffect : MonoBehaviour
         msgText.text += targetMsg[index]; //문자열 배열처럼 접근 가능!
         index++;
 
-        Invoke("Effecting", interval); //재귀함수
+        Invoke("Effecting", 1 / CharPerSeconds); //재귀함수
     }
 
     void EffectEnd()
