9e086d015ccd36978f2de82137f6dab4764c5537
gkdmsgus <gkdmsgus0422@gmail.com>
2024-01-17 20:32:48 +0900

    test3
    toolselector change

--- Assets/ToolSelector.cs
+++ Assets/ToolSelector.cs
@@ -1,64 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ToolSelector : MonoBehaviour
 {
-    public ToolSlot[] toolSlots; // 도구 슬롯을 담는 배열
-    public Sprite[] toolImages; // 도구 이미지를 담는 배열
-    public int selectedToolIndex = 0; // 현재 선택된 도구의 인덱스
+    public GameObject[] toolSlots = new GameObject[3]; 
+    public Sprite[] toolImages = new Sprite[3]; 
+    private int selectedToolIndex = 0;
 
     private void Start()
     {
-        for (int i = 0; i < toolSlots.Length; i++)
-        {
-            toolSlots[i].SetImage(toolImages[i]); // 각 슬롯에 이미지 설정
-        }
-
-        SelectTool(selectedToolIndex); // 초기 도구 선택
+        SelectTool(0); // 게임 시작 시 첫 번째 도구를 선택
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1)) // 1번 키를 눌렀을 때
+        // 도구 선택
+        for (int i = 0; i < toolSlots.Length; i++)
         {
-            SelectTool(0); // 1번 도구 선택
+            if (Input.GetKeyDown(KeyCode.Alpha1 + i))
+            {
+                SelectTool(i);
+            }
         }
-        else if (Input.GetKeyDown(KeyCode.Alpha2))
+
+        if (Input.GetKeyDown(KeyCode.Space)) // 스페이스 키를 눌렀을 때
         {
-            SelectTool(1);
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            SelectTool(2);
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha4))
-        {
-            SelectTool(3);
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha5))
-        {
-            SelectTool(4);
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha6))
-        {
-            SelectTool(5);
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha7))
-        {
-            SelectTool(6);
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha8))
-        {
-            SelectTool(7);
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha9))
-        {
-            SelectTool(8);
+            UseTool(selectedToolIndex); // 현재 선택된 도구 사용
         }
     }
 
+    // 도구를 선택하는 메소드
     private void SelectTool(int index)
     {
         if (index < 0 || index >= toolSlots.Length)
@@ -80,5 +51,14 @@ public class ToolSelector : MonoBehaviour
             toolImage.sprite = toolImages[index];
 
         selectedToolIndex = index;
+    }
+
+    // 도구를 사용하는 메소드
+    private void UseTool(int index)
+    {
+        if (index < 0 || index >= toolSlots.Length)
+            return;
+
+        // 도구 사용 코드
     }
 }
