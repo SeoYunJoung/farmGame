777ea9c6f1aaf4aae20dcf453e68d3708a9c1fca
Seo-YunJoung <130148628+SeoYunJoung@users.noreply.github.com>
2024-01-18 16:17:22 +0900

    Revert "18일 회의 이전 파일"
    This reverts commit 06d39ef3ebd803ae2fcce0991da85a6f7c63034a.

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -13,8 +13,6 @@ public class GameManager : MonoBehaviour
     public TalkManager talkManager;
     public GameObject talkPanel;
     public Image portraitImg;
-    public Sprite prevPortraitImg;
-    public Animator portAnim;
     public TypeEffect talk;
     public GameObject scanObject;
     public bool talkAction;
@@ -60,15 +58,8 @@ public class GameManager : MonoBehaviour
             return; //함수 종료
         }
 
-        talk.SetMsg(talkData.Split(':')[0]); //대화 출력
-        portraitImg.sprite = talkManager.GetPortrait
-            (id, int.Parse(talkData.Split(":")[1])); //초상화 출력
-        if(prevPortraitImg != portraitImg.sprite)
-        {
-            portAnim.SetTrigger("doEffect");
-            prevPortraitImg = portraitImg.sprite;
-            portAnim.SetTrigger("doEffect");
-        }
+        talk.SetMsg(talkData);
+        //portraitImg.color = new Color(1, 1, 1, 1);
 
         talkAction = true;
         talkIndex++;
